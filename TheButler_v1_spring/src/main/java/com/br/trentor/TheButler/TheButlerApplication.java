5bb194582b3bcf83980be61ddfcbc2940d640f97
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2024-05-06 17:45:51 -0300

    Dcomentation Created, and Spring Project Created!

---
+++ TheButler_v1_spring/src/main/java/com/br/trentor/TheButler/TheButlerApplication.java
@@ -0,0 +1,13 @@
+package com.br.trentor.TheButler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheButlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheButlerApplication.class, args);
+	}
+
+}
