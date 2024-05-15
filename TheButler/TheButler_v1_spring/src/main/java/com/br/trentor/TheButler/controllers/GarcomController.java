0b2120aec53490b1e86a22e48f437f49e0d9e947
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2024-05-15 02:59:35 -0300

    Dcomentation Hibernate

--- TheButler/TheButler_v1_spring/src/main/java/com/br/trentor/TheButler/controllers/GarcomController.java
+++ TheButler/TheButler_v1_spring/src/main/java/com/br/trentor/TheButler/controllers/GarcomController.java
@@ -13,9 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.trentor.TheButler.model.dtos.GarcomDTO;
+import com.br.trentor.TheButler.model.dtos.GarcomDTO;
 import com.br.trentor.TheButler.services.impl.GarcomServicesImpl;
-import com.br.trentor.TheButler.services.impl.UsuarioServicesImpl;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -26,16 +25,30 @@ public class GarcomController {
 
 	@Autowired
 	private GarcomServicesImpl garcomServices;
-	
-	
+
 	@Operation(tags = "Garçom", summary = "Cria um novo garçom no banco", description = "Faz a criação de um tipo garçom dentro do banco")
 	@PostMapping(path = "registrar-garcom")
 	public ResponseEntity<GarcomDTO> criarUsuario(@RequestBody GarcomDTO novoUsuario) throws Exception {
 		GarcomDTO usuarioCadastrado = garcomServices.registrarNovoGarcom(novoUsuario);
 		return new ResponseEntity<>(usuarioCadastrado, HttpStatus.CREATED);
 	}
-	
-	//IMPLEMENTAR RESTANTE DO CRUD
-	
-	//TERA OUTRAS FUNCÇÕES E SERA IMPLEMENTADO SPRING SECURITY POIS É UMA CLASSE QUE MEXE DIRETO NOS USUARIOS DO BANCO
+
+	@Operation(tags = "Garçom", summary = "Atualiza um usuario existente no banco", description = "Faz a atualização de um tipo qualquer dentro do banco")
+	@PutMapping(path = "atualizar-usuario")
+	public ResponseEntity<GarcomDTO> atualizarUsuarioExistente(@RequestBody GarcomDTO usuarioExistente)
+			throws Exception {
+		var user = garcomServices.atualizarDadosGarcomExistente(usuarioExistente);
+		return new ResponseEntity<>(user, HttpStatus.OK);
+
+	}
+
+	@Operation(tags = "Garçom", summary = "Deleta um usuario no banco", description = "Faz a deleção de um tipo qualquer dentro do banco")
+	@DeleteMapping(path = "deletar-usuario/{id}")
+	public ResponseEntity<?> deletarUsuarioPeloId(@PathVariable Long id) throws Exception {
+		garcomServices.deletarGarcomPorId(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	// TERA OUTRAS FUNCÇÕES E SERA IMPLEMENTADO SPRING SECURITY POIS É UMA CLASSE
+	// QUE MEXE DIRETO NOS Garçom DO BANCO
 }
--- TheButler/TheButler_v1_spring/src/main/java/com/br/trentor/TheButler/controllers/UsuarioController.java
+++ TheButler/TheButler_v1_spring/src/main/java/com/br/trentor/TheButler/controllers/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,6 +27,7 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioServicesImpl userServices;
 	
+	@GetMapping(path = "procurar-por-id/{id}")
 	public ResponseEntity<UsuarioDTO> obterUsuarioPorId(@PathVariable Long id) throws Exception {
 		UsuarioDTO user = userServices.buscarUsuarioPorId(id);
 		return new ResponseEntity<>(user, HttpStatus.OK);
